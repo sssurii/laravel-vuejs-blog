05edec3a8f9f07b6cb41b5b288e73fe4fe6753c5
Surinder Singh <sssurii.dev@gmail.com>
2020-07-13 21:52:59 +0530

    add new component navbar - demostrating for loop in vue

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="links">
+        <a v-for="link in links" v-bind:href="link.link">{{link.title}}</a>
+    </div>
+</template>
+<script>
+    export default {
+        props : ['links'],
+        mounted() {
+            console.log('navbar loaded')
+        }
+    }
+</script>
