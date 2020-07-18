a9616ab6eb3b7b3a0fd19bf8a9793ba0d454f3a4
Surinder Singh <sssurii.dev@gmail.com>
2020-07-18 21:58:13 +0530

    update Post factory and Seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,10 +3,19 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Post;
+use App\User;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$factory->define(Post::class, function (Faker $faker, $attributes) {
+    $user_id = $attributes['user_id'] ?? (factory(User::class)->create())->id;
     return [
-        //
+        'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'content' => $faker->realText($faker->numberBetween(10, 200)),
+        'user_id' => $user_id,
+        'meta' => [
+            'tags' => $faker->words($nbWords = 3, $asText = false)
+        ],
+        'status' => 'Published',
+        'published_at' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now', $timezone = null)
     ];
 });
