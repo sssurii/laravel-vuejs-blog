b383e10490d73871c090b36edfe5a932c53c6806
Surinder Singh <sssurii.dev@gmail.com>
2020-07-18 19:47:19 +0530

    demostrating using input and binding to data variable

--- resources/js/components/HelloWorld.vue
+++ resources/js/components/HelloWorld.vue
@@ -2,11 +2,18 @@
     <div>
         <h1>{{title}}</h1>
         <p>{{content}}</p>
+        <input type="text" v-model="name">
+        <p>You are writing : {{name}}</p>
     </div>
 </template>
 <script>
     export default {
         props: ['title', 'content'],
+        data () {
+            return {
+                name: ''
+            }
+        },
         mounted() {
             console.log('Component Hello World mounted.')
         }
