0162fbcae09c14c90613aa82c1ab7710a65f7d7a
Surinder Singh <sssurii.dev@gmail.com>
2020-07-21 21:13:21 +0530

    Initial setup for Testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,23 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public static $is_migrated = false;
+
+    public static function setUpBeforeClass():void
+    {
+        if (!self::$is_migrated) {
+            echo "Running migrations...\n";
+            exec('php artisan migrate:fresh');
+            echo "Seeding database...\n ";
+            exec('php artisan db:seed');
+            self::$is_migrated = true;
+        }
+    }
+
+    public function tearDown(): void
+    {
+        \DB::connection()->setPdo(null);
+        parent::tearDown();
+    }
 }
