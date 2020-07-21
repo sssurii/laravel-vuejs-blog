4d05978285b56f8be0ac5e70296655dc14b77cc7
Surinder Singh <sssurii.dev@gmail.com>
2020-07-21 23:01:14 +0530

    add app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{url('/css/app.css?v='.rand(), [], env('APP_SSL'))}}">
+        <link rel="stylesheet" type="text/css" href="{{url('/css/custom.css?v='.rand(), [], env('APP_SSL'))}}">
+    </head>
+    <body>
+        <div id="app" class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <navbar-menu></navbar-menu>
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+        <script type="text/javascript" src="{{url('/js/app.js?v='.rand(), [], env('APP_SSL'))}}"></script>
+    </body>
+</html>
