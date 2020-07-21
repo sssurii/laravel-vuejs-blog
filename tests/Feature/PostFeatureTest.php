588507f55efd8d7ec7baa5b12704504c36b761d7
Surinder Singh <sssurii.dev@gmail.com>
2020-07-21 22:43:09 +0530

    add Post Feature Test class and write simple feature test for getting posts list

---
+++ tests/Feature/PostFeatureTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostFeatureTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testPostsListApi()
+    {
+        $response = $this->get('/posts');
+
+        $response->assertStatus(200);
+    }
+}
