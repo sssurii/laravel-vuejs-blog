68066b6a43d91ad086b1591bf36ce19b02b419ab
Surinder Singh <sssurii.dev@gmail.com>
2020-07-21 23:04:01 +0530

    separate header part from layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,20 +13,7 @@
     </head>
     <body>
         <div id="app" class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-            <navbar-menu></navbar-menu>
+            @include('layouts.header')
             <div class="content">
                 @yield('content')
             </div>
