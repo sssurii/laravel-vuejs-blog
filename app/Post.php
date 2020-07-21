f2b12b1319ddc49dd73dca0b496c94436962e2e8
Surinder Singh <sssurii.dev@gmail.com>
2020-07-21 21:58:17 +0530

    add model method to list posts from DB

--- app/Post.php
+++ app/Post.php
@@ -19,4 +19,10 @@ class Post extends Model
     protected $casts = [
         'meta' => 'json',
     ];
+
+    public function list()
+    {
+        return $this->orderBy('published_at', 'desc')
+                    ->get();
+    }
 }
