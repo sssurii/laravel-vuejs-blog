f93b24d50833102a46b2b08c6dcaea7c792a3e7e
Surinder Singh <sssurii.dev@gmail.com>
2020-07-22 21:35:57 +0530

    user of v-if and v-else

--- resources/js/components/Blog.vue
+++ resources/js/components/Blog.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container">
         <h1>Blog</h1>
-        <blog-post v-for="post in blogPosts" v-bind:post="post" v-bind:key="post.id"></blog-post>
+        <div v-if="blogPosts"> 
+            <!-- It's not recommend to use v-if with v-for. For more detail check :
+                https://vuejs.org/v2/style-guide/#Avoid-v-if-with-v-for-essential -->
+            <blog-post v-for="post in blogPosts" v-bind:post="post" v-bind:key="post.id"></blog-post>
+        </div>
+        <div v-else>
+            <p>Sorry! no post available</p>
+        </div>
     </div>
 </template>
 <script>
