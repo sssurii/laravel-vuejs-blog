b68e706d16d6378486b64283804b3803ad5fed2f
Surinder Singh <sssurii.dev@gmail.com>
2020-08-22 15:30:18 +0530

    catching Dynamic URL param, using it to fetch post content. Note: while using same component, fetch post details when no props Post details are passed i.e. viewing a single post

--- resources/js/components/BlogPost.vue
+++ resources/js/components/BlogPost.vue
@@ -7,6 +7,13 @@
 <script>
     export default {
         props: ['post'],
+        beforeMount() {
+            if(this.post == undefined) {
+                let post_id = this.$route.params.id;
+                axios.get('http://blog.laravel-vuejs.com/api/post'+ '/' + post_id)
+                .then(response => (this.postObj = response.data));
+            }
+        },
         mounted() {
             console.log('Component Blog Post mounted.')
         }
