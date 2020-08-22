b9d940661ae89811d61b705ebceff50dec812444
Surinder Singh <sssurii.dev@gmail.com>
2020-08-22 15:41:30 +0530

    add a readmore link to post on home page, show link conditionally only when post shown in home page i.e. props post is passed

--- resources/js/components/BlogPost.vue
+++ resources/js/components/BlogPost.vue
@@ -2,16 +2,23 @@
     <div>
          <h1>{{post.title}}</h1>
          <p>{{post.content}}</p>
+         <router-link v-if="showReadMore" :to="'post/' + post.id" :key="post.title">Read More ...</router-link>
     </div>
 </template>
 <script>
     export default {
         props: ['post'],
+        data() {
+            return {
+                showReadMore: true,
+            }
+        },
         beforeMount() {
             if(this.post == undefined) {
+                this.showReadMore = false;
                 let post_id = this.$route.params.id;
                 axios.get('http://blog.laravel-vuejs.com/api/post'+ '/' + post_id)
-                .then(response => (this.postObj = response.data));
+                .then(response => (this.post = response.data));
             }
         },
         mounted() {
