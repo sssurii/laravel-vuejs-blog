154cb4fe8e09c6af0073f2c9ee0ef037b78af369
Surinder Singh <sssurii.dev@gmail.com>
2020-08-22 15:48:22 +0530

    let's create view post api - add route

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('posts', 'PostController@index');
+Route::get('/post/{id}', 'PostController@show');
