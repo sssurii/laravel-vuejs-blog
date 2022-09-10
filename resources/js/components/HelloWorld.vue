7f8085e415421d40fa7597f9e3d57ce9aee8dbc8
Surinder Singh <sssurii.dev@gmail.com>
2022-09-10 22:55:41 +0530

    fix console error in Helloworld component

--- resources/js/components/HelloWorld.vue
+++ resources/js/components/HelloWorld.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{customTitle}}</h1>
         <p>{{content}}</p>
-        <input placeholder="Enter your name" type="text" v-model="name" v-on:change="showName">
+        <input placeholder="Enter your name" type="text" v-model="name" v-on:change="showName" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
         <p>You are writing : {{name}}</p>
     </div>
 </template>
@@ -19,7 +19,7 @@
         },
         computed:{
             customTitle: function () {
-                return (this.name == '') ? this.title : this.title.split(' ')[0] + ` ${this.name}!`;
+                return (this.name == '') ? this.title : (this.title || "").split(' ')[0] + ` ${this.name}!`;
             }
         },
         methods: {
