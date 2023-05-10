9648cfedd98ac4f59bdee529d560c8d9fcba7db6
Surinder Singh <sssurii.dev@gmail.com>
2023-05-10 11:23:03 +0530

    update register form - fix syntex error, fix route method and spacing

--- resources/js/components/RegisterForm.vue
+++ resources/js/components/RegisterForm.vue
@@ -2,7 +2,7 @@
     <div class="row flex-column">
         <div class="col-md-4 offset-md-4">
             <div v-if="message">
-                <div v-if"success" class="alert alert-success" role="alert">
+                <div v-if="success" class="alert alert-success" role="alert">
                   {{message}}
                 </div>
                 <div v-else class="alert alert-danger" role="alert">
@@ -15,23 +15,23 @@
                 </ul>
             </div>
 
-            <form class="form-signup form" method="post" v-bind:action="url('/register')">
+            <form class="form-signup form mb-3" method="post" v-bind:action="url('/register')">
                 <h1 class="h3 mb-3 font-weight-normal">Register</h1>
-                <div class="form-group">
-                    <label for="inputEmail" class="sr-only">Name</label>
+                <div class="form-group mb-3">
+                    <label for="name" class="sr-only">Name</label>
                     <input type="text" id="name" name="name" class="form-control" placeholder="Name" required="" autofocus="">
                 </div>
-                <div class="form-group">
-                    <label for="inputEmail" class="sr-only">Email address</label>
+                <div class="form-group mb-3">
+                    <label for="email" class="sr-only">Email address</label>
                     <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required="" >
                 </div>
-                <div class="form-group">
-                    <label for="inputPassword" class="sr-only">Password</label>
+                <div class="form-group mb-3">
+                    <label for="password" class="sr-only">Password</label>
                     <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
                 </div>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
             </form>
-            <a v-bind:href="route('login')">Login</a>
+            <a v-bind:href="url('/login')">Login</a>
         </div>
     </div>
 </template>
@@ -46,8 +46,8 @@
             };
         },
         methods: {
-            url: (link) => window.location.host + link, 
-            route: (link) => window.location.host + link
+            url: (link) => window.location.origin + link, 
+            route: (link) => window.location.origin + link
         },
         mounted() {
             console.log('Component Register Form mounted.')
